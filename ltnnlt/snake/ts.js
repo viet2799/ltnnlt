8b88b43de552281133cc19b526a54694cbe7279c
viet2799 <nguyenducviet2799@gmail.com>
2020-07-14 00:44:33 +0700

    update lần cuối

---
+++ ltnnlt/snake/ts.js
@@ -0,0 +1,14 @@
+// const localtunnel = require('localtunnel');
+
+// const tunnel = localtunnel(port, { subdomain: 'ltnnt'} (err, tunnel) => {
+//      console.log('hello')
+// }); 
+
+// tunnel.on('close', function() { 
+//     // When the tunnel is closed
+//     console.log('end');
+// });
+
+
+
+// tunnel.close();
